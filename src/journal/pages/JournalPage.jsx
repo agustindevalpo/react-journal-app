ee832d0712860942b1afbe54197864a6573c733f
agustindevalpo <agustin.romero@devalpo.cl>
2025-04-23 19:06:03 -0400

    Configuración inicial
    Configuración inicial del Redux y su reducer

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,34 +1,33 @@
-
-import { IconButton } from '@mui/material'
-import { JournalLayout } from '../layout/JournalLayout'
-import { NoteView, NothingSelectedView } from '../views'
-import { AddOutlined } from '@mui/icons-material'
-
+import { IconButton } from '@mui/material';
+import { JournalLayout } from '../layout/JournalLayout';
+import { NoteView, NothingSelectedView } from '../views';
+import { AddOutlined } from '@mui/icons-material';
 
 export const JournalPage = () => {
   return (
     <>
-
       <JournalLayout>
         <NothingSelectedView />
         {/* <NoteView /> */}
       </JournalLayout>
 
       <IconButton
-          size='large'
-          sx ={{
-            color: 'white',
-            backgroundColor: 'error.main',
-            ':hover': { backgroundColor: 'error.main', opacity: 0.9 },
-            position: fixed,
-            right: 50,
-            bottom: 50
-          }}>
-
-         <AddOutlined sx = {{ fontSize: 30}}/>    
+        size='large'
+        sx={{
+          color: 'white',
+          backgroundColor: 'error.main',
+          '&:hover': { 
+            backgroundColor: 'error.main', 
+            opacity: 0.9 
+          },
+          position: 'fixed', // Corregido: como string
+          right: 50,
+          bottom: 50,
+          zIndex: 999 // Asegura que esté por encima de otros elementos
+        }}
+      >
+        <AddOutlined sx={{ fontSize: 30 }} />    
       </IconButton>
-
     </>
-
-  )
-}
+  );
+};
