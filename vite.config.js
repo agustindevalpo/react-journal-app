b22103d53caf6a9f9aaa7b32c6ab1aa5a081f327
agustindevalpo <agustin.romero@devalpo.cl>
2025-04-30 10:31:44 -0400

    Fix

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,20 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', 
+  base: './', // Esto es crucial
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name]-[hash][extname]',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        entryFileNames: 'assets/[name]-[hash].js'
+      }
+    }
+  },
+  server: {
+    historyApiFallback: true
   }
 })
