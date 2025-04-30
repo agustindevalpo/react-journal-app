985d91a31c691aa0ca4f3628f67dfd65b2acb0da
agustindevalpo <agustin.romero@devalpo.cl>
2025-04-30 14:00:49 -0400

    Fix 4.5.1.1

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Esto es crucial
+  base: '/', // Esto es crucial
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
