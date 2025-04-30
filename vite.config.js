b9b107b13e4597a0352efa6de9e33a994bcd143f
agustindevalpo <agustin.romero@devalpo.cl>
2025-04-30 10:17:29 -0400

    Summary Vercel

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/', 
   build: {
-    outDir: 'dist',  // Vercel buscará esta carpeta por defecto
-    sourcemap: false, // Opcional: desactiva sourcemaps para producción
-  },
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true
+  }
 })
